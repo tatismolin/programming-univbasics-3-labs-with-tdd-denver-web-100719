a737bde7571c15fc9a4051673c0f66c5a8347f18
Tatiana Smolin <tatismolin@gmail.com>
2019-09-15 21:20:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,20 +1,18 @@
+first_number = gets.to_i
+second_number = gets.to_i
+
 def sum (first_number, second_number)
-  first_number = 4
-  second_number = 2
-  sum = first_number + second_number
+  return first_number + second_number
 end
+
 def difference (first_number, second_number)
-  first_number = 4
-  second_number = 2
-  difference = first_number - second_number
+  return first_number - second_number
 end
+
 def product (first_number, second_number)
-  first_number = 4
-  second_number = 2
-  product = first_number * second_number
+  return first_number * second_number
 end
+
 def quotient (first_number, second_number)
-  first_number = 4
-  second_number = 2
-  quotient = first_number / second_number
+  return first_number / second_number
 end
