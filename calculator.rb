77033b462f68f3595a21c2b3c888807aa4bd18d8
Tatiana Smolin <tatismolin@gmail.com>
2019-09-15 21:29:23 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = gets.to_i
-second_number = gets.to_i
+first_number =
+second_number =
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
