3c15a5eeecfffaadaa0da26666f06a5970dc0aa3
Tatiana Smolin <tatismolin@gmail.com>
2019-09-15 21:16:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,18 +1,20 @@
-# Add your variables here
-first_number = 4
-second_number = 2
-
 def sum (first_number, second_number)
   first_number = 4
   second_number = 2
   sum = first_number + second_number
 end
 def difference (first_number, second_number)
+  first_number = 4
+  second_number = 2
   difference = first_number - second_number
 end
 def product (first_number, second_number)
+  first_number = 4
+  second_number = 2
   product = first_number * second_number
 end
 def quotient (first_number, second_number)
+  first_number = 4
+  second_number = 2
   quotient = first_number / second_number
 end
