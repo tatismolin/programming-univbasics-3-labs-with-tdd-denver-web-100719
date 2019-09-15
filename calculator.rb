c23d78455e0731f1a21e7a34d174fb17a7a7fcae
Tatiana Smolin <tatismolin@gmail.com>
2019-09-15 21:28:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,23 +1,8 @@
 first_number = gets.to_i
 second_number = gets.to_i
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
 
-def sum(first_number, second_number)
-  return first_number + second_number
-end
 
-def difference(first_number, second_number)
-  return first_number - second_number
-end
-
-def product(first_number, second_number)
-  return first_number * second_number
-end
-
-def quotient(first_number, second_number)
-  return first_number / second_number
-end
-
-sum(first_number, second_number)
-difference(first_number, second_number)
-product(first_number, second_number)
-quotient(first_number, second_number)
